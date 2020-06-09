7e9ec94cd4aa73487af67070bec19b1e3cb22f8e
Eseodrone <eseodrone@gmail.com>
2020-06-09 14:50:37 +0200

    maj et code des ToF

--- software/appli/main.c
+++ software/appli/main.c
@@ -16,6 +16,8 @@
 #include "MPU6050/stm32f4_mpu6050.h"
 #include "systick.h"
 #include "init_stm32f4.h"
+#include "pixy.h"
+
 
 
 int main(void){
@@ -26,20 +28,22 @@ int main(void){
 	UART_init();
 
 	BSP_GPIO_PinCfg(LEDS_GPIO, LED_GREEN_PIN | LED_ORANGE_PIN | LED_RED_PIN | LED_BLUE_PIN, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, GPIO_SPEED_FAST, 0);
+	BSP_GPIO_PinCfg(GPIOA, GPIO_PIN_7, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, GPIO_SPEED_FAST, 0);
 
 	Systick_init();
+
 
 	//HAL_GPIO_WritePin(LEDS_GPIO, LED_GREEN_PIN | LED_ORANGE_PIN, SET);
 	HAL_GPIO_WritePin(LEDS_GPIO, LED_RED_PIN | LED_BLUE_PIN, SET);
 
+	//pixy_init();
+
+	//init_pwm_tim1_tim3();
+
+	//TEST_pwms();
 
 
-	init_pwm_tim1_tim3();
-
-	TEST_pwms();
-
-
-	//VL53L1X_init();
+	VL53L1X_init();
 
 	/*
 	MPU6050_t mpu6050_datastruct;
@@ -48,18 +52,31 @@ int main(void){
 		mpu6050_initialized = TRUE;
 	*/
 
-	//TIMER2_run_1ms();
+	TIMER2_run_1ms();
+
+
+	static uint16_t dist = 0;
+
 
 
 	while (1)
 	{
+		dist = VL53L1X_get_distance(0);
 
-		//VL53L1X_process_main();
+		if(dist < 100){
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, SET);
+		}
+		else if(dist > 100){
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, RESET);
+		}
+
+		//pixy_process_main();
+
 
 	}
 }
 
-/*
+
 //IT 1ms, de plus basse priorité que l'IT du systick !
 void TIMER2_user_handler_it_1ms(void)
 {
@@ -71,7 +88,7 @@ void TIMER2_user_handler_it_1ms(void)
 	//notamment pour discuter avec le MPU6050 ! (car il utilise le même bus I2C ! donc il faut uniquement lui causer en IT !)
 }
 
-*/
+
 
 
 
