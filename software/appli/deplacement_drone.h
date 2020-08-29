239e3f6b6ce2a97f908c8d11bccca7a95141e6cd
Skyflowed <62890017+Skyflowed@users.noreply.github.com>
2020-08-29 17:14:12 +0200

    ataTests et ajout de fonctions data_process

--- software/appli/deplacement_drone.h
+++ software/appli/deplacement_drone.h
@@ -10,11 +10,33 @@
 
 #include "datas_process.h"
 
+#define MINOR_ANGLE		5
+//#define MAXIMAL_POWER
+
+typedef enum
+{
+	FIXATION_INIT,
+	FIXING,
+	FIXED,
+}fixes_e;
+
+typedef enum
+{
+	NEED_FIXATION = 0,
+	FIXATION_IN_PROGRESS,
+	FIXATION_OK,
+	FIXATION_TIMEOUT, //Au delà de 3 sec on recalcule ?
+};
+
 typedef enum{
 	TRANS_Z_HAUT = 0,
 	TRANS_Z_BAS
 }trans_z_e;
 
+<<<<<<< Updated upstream
+=======
+void DD_mode_management(void);
+>>>>>>> Stashed changes
 
 
 #endif /* APPLI_DEPLACEMENT_DRONE_H_ */
