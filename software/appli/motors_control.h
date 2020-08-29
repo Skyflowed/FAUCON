f68aa9e117747cead0616f7b614de23b6776ab00
Skyflowed <62890017+Skyflowed@users.noreply.github.com>
2020-08-29 11:33:00 +0200

    Ajout code position + approche moteurs

---
+++ software/appli/motors_control.h
@@ -0,0 +1,16 @@
+/**
+  ******************************************************************************
+  * @file    motors_control.h
+  * @author  Flo
+  * @date    25-August-2020
+  ******************************************************************************
+*/
+#ifndef MOTORS_CONTROL_H
+	#define MOTORS_CONTROL_H
+
+#define MINOR_ANGLE		5
+//#define MAXIMAL_POWER
+
+void MC_mode_management(void);
+
+#endif
